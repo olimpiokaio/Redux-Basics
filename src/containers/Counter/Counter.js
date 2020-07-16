addbe8084d9c4336f352a508b4dcb378262d763c
olimpio home <olimpiocode@gmail.com>
2020-07-16 12:33:40 -0300

    Adicionado lista de resultados

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -13,6 +13,15 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storeResult.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>
+                            {strResult.value}
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +29,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storeResult: state.results
     }
 }
 
@@ -29,7 +39,9 @@ const mapDispatchToProps = dispatch => {
         onIcrementCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', val: 10}),
-        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15})
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 }
 
